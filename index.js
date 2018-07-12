0e00d6127cc2fcccb780541f5fcc4a26ee2f1a08
Artem Pastukhov <artem.pastukhov@gmail.com>
2018-07-12 15:47:40 +0300

    rework uri and error handling

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-var request = require('request');
-var methods = require('./api-methods');
+var request = require("request");
+var methods = require("./api-methods");
 
 function serializeObjToUri(obj) {
-  return Object.keys(obj).map(function(key) {
-    return key + '=' + encodeURIComponent(obj[key]);
-  }).join('&');
-}
-
-function buildBasicAuthHeader(obj) {
-  return 'Basic ' + new Buffer(obj.username + ':' + obj.password).toString('base64');
+  return Object.keys(obj)
+    .map(function(key) {
+      return key + "=" + encodeURIComponent(obj[key]);
+    })
+    .join("&");
 }
 
 var Api = function(config) {
-  this._protocol = config.protocol || 'http'; // or https
-  this._auth = (config.basicAuth) ? buildBasicAuthHeader(config.basicAuth) : '';
-  this._host = config.host || 'localhost';
-  this._port = config.port || '12900';
-  this._path = config.path || '';
-  this._uri = this._protocol + '://' + this._host + ':' + this._port + this._path;
+  this._uri = this.uri;
 };
 
 Object.keys(methods).forEach(function(mName) {
-
   var m = methods[mName];
 
   Api.prototype[mName] = function(parameters, path, callback) {
-
     if (arguments.length === 1) callback = parameters;
     if (arguments.length === 2) callback = path;
 
     var computedPath = m.path;
-    if (typeof arguments[1] === 'object') {
+    if (typeof arguments[1] === "object") {
       computedPath = m.path.replace(/{([^}]*)}/g, function(s, p) {
         return path[p];
       });
@@ -38,41 +29,35 @@ Object.keys(methods).forEach(function(mName) {
 
     var reqUri = this._uri + computedPath;
 
-    if (m.method === 'GET' && parameters) {
-      reqUri = reqUri + '?' + serializeObjToUri(parameters);
+    if (m.method === "GET" && parameters) {
+      reqUri = reqUri + "?" + serializeObjToUri(parameters);
     }
 
     var opts = {
       url: reqUri,
       method: m.method,
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: this._auth
+        Accept: "application/json",
+        "Content-Type": "application/json"
       },
-      body: (m.method !== 'GET' && parameters) ? parameters : null,
+      body: m.method !== "GET" && parameters ? parameters : null,
       json: false
     };
     request(opts, function(error, response, body) {
+      if (body === "") body = "{}";
       if (error) {
         return callback([error, body]);
       }
-
-      try {
-        /*
-        removeStream() will respond an empty body;
-        we need a fallback to prevent JSON.parse(body) to fail
-         */
-        if(body === '')
-            body = '{}';
-        callback(null, JSON.parse(body));
+      if (response.statusCode === 403) {
+        return callback([JSON.parse(body).message, body]);
       }
-      catch (err) {
-        callback(['Bad response', err, reqUri]);
+      try {
+        callback(null, JSON.parse(body));
+      } catch (err) {
+        callback(["Bad response", err, reqUri]);
       }
     });
   };
-
 });
 
 var connect = function(config, callback) {
